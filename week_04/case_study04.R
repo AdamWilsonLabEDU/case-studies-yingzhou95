d96d1be64e51eddd290f57914f837467f179471e
Ying Zhou <yzhou59@buffalo.edu>
2024-09-24 17:05:54 -0400

    case study 04 initial coding

---
+++ week_04/case_study04.R
@@ -0,0 +1,11 @@
+# load packages
+#install.packages("nycflights13")
+library(tidyverse)
+library(nycflights13)
+
+# look at contens of each table in the dataset
+head(airlines)
+head(airports)
+head(flights)
+head(planes)
+head(weather)
