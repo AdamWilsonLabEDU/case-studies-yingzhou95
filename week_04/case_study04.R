952077afb9388bd82c01f4db261218e5331bb6fd
Ying Zhou <yzhou59@buffalo.edu>
2024-09-24 17:10:12 -0400

    case study 04 initial coding

--- week_04/case_study04.R
+++ week_04/case_study04.R
@@ -3,9 +3,26 @@
 library(tidyverse)
 library(nycflights13)
 
-# look at contens of each table in the dataset
-head(airlines)
-head(airports)
-head(flights)
-head(planes)
-head(weather)
+# look at contents of each table in the dataset
+view(airlines)
+view(airports)
+view(flights)
+view(planes)
+view(weather)
+
+# find the destination airport farthest from any of the NYC airports
+farthest_airport <- flights %>% 
+  filter(dest %in% c("JFK", "LGA", "EWR", "SWF", "ISP")) %>%
+  arrange(desc(distance)) %>%
+  left_join(airports, by = c("dest" = "faa" )) %>%
+  slice(1) %>%
+  select(name)
+
+view(farthest_airport)
+
+
+
+
+
+
+
