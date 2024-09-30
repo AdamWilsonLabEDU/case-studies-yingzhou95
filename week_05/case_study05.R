b69dc73d44e8b392b44d629edcc2458151f9f3d9
Ying Zhou <yzhou59@buffalo.edu>
2024-09-30 17:00:58 -0400

    with leaflet map

--- week_05/case_study05.R
+++ week_05/case_study05.R
@@ -53,6 +53,15 @@ ggplot() +
 install.packages("leaflet")
 library(leaflet)
 
+# Convert border_area to a format compatible with leaflet
+border_area_leaflet <- st_transform(border_area, crs = 4326)
+
+leaflet(border_area_leaflet) %>%
+  addTiles(urlTemplate = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png") %>%
+  addPolygons(color = "black",
+              weight = 1,
+              fillColor = "red",
+              fillOpacity = 0.5)
 
 
 
